fc55f9af63b35e8b4a630bf6097e19dd46deea99
annblessy <annblessy2004@gmail.com>
2025-05-06 09:01:27 +0530

    Initial commit

---
+++ RideOnDurr/src/main/java/com/example/RideOnDurr/RideOnDurrApplication.java
@@ -0,0 +1,13 @@
+package com.example.RideOnDurr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideOnDurrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RideOnDurrApplication.class, args);
+	}
+
+}
---
+++ RideOnDurr/src/test/java/com/example/RideOnDurr/RideOnDurrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RideOnDurr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideOnDurrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
