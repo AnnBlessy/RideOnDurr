e9fe56ae096e4bb9eaac88a433c349397b1cc5f9
PSriVarshan <psvvarshan@gmail.com>
2025-05-06 10:38:15 +0530

    Added CarTypes and Ride Ratings Entities

---
+++ RideOnDurr/src/main/java/com/example/RideOnDurr/Entity/CarType.java
@@ -0,0 +1,29 @@
+package com.example.RideOnDurr.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class CarType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer CarTypeId;
+    private String CarType;
+    private Double farePerKM;
+    private Integer maxCapacity;
+
+    public CarType(Integer carTypeId, String carType, Double farePerKM, Integer maxCapacity) {
+        CarTypeId = carTypeId;
+        CarType = carType;
+        this.farePerKM = farePerKM;
+        this.maxCapacity = maxCapacity;
+    }
+
+    public CarType() {
+    }
+
+}
---
+++ RideOnDurr/src/main/java/com/example/RideOnDurr/Entity/RideRatings.java
@@ -0,0 +1,22 @@
+package com.example.RideOnDurr.Entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class RideRatings {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long ratingId;
+    /* Need to addd booking ID */
+    private Integer stars;
+    @Column(nullable = true)
+    private String title;
+    @Column(nullable = true)
+    private String details;
+}
